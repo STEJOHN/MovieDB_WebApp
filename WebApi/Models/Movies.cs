235e99e4ed07d814deb3a1ae33fc24f9e087e023
Stephen Johnson <stephenjayyy@gmail.com>
2021-10-27 16:14:13 -0500

    Initial commit

---
+++ WebApi/Models/Movies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+    }
+}
